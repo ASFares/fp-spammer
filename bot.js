caa10acf79039963f346548246d1d928ae3db031
ASFares <38036378+ASFares@users.noreply.github.com>
2018-11-30 23:12:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,6 +33,22 @@ client.on('message', message => {
     }
 });
 
+client.on('message', async msg => {
+const devs = ['315296879019687937'];
+if(!devs.includes(message.author.id)) return;
+  if(msg.author.bot) return;
+  let prefix = "$";
+  if(!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0].slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+ 
+  if(command == 'say') {
+    if(!args.join(" ")) return msg.delete();
+    msg.channel.send(args.join(" "));
+    return;
+  }
+});
+
 client.on('message', message => {
     if(message.content === '-ريب'){
         message.channel.send('#credits')
